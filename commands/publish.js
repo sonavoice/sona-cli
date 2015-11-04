f2ce0fce0103b02eae0f2a8a9cb5162dce012720
Christopher Yung <cyung10892@gmail.com>
2015-11-03 17:18:35 -0800

    update file checking on publish

--- commands/publish.js
+++ commands/publish.js
@@ -15,55 +15,41 @@ module.exports.run = function(args) {
 
   console.log("Publishing " + name.yellow + "...");
 
-  var filename = process.cwd() + '/extension.js';
-  fs.readFile(filename, 'utf-8', function(err, data) {
-    if (err) {
-      console.log('err =', err);
-      console.log('Could not read extension.js');
-      return;
-    }
+  var ext;
+  try {
+    ext = require(process.cwd() + '/extension');
+  } catch(e) {
+    console.log('Unable to load file. Please check your syntax.'.red);
+    console.log(e);
+    return;
+  }
 
-    try {
-      var obj = JSON.parse(data);
-      if (!isValidFile(obj)) {
-        console.log('You are missing properties in your extension.'.red);
-        return;
+  if (!isValidFile(ext)) {
+    return;
+  }
+
+  utils.zip('.', name, function() {
+
+    var host;
+    var demo = false;
+
+    host = (demo) ? 'http://localhost:3000' : 'https://sonavoice.com';
+
+    var formData = {
+      guid: utils.getGUID(),
+      extension: fs.createReadStream(utils.lodir(name + '.zip')),
+      email: utils.getEmail(),
+      name: name,
+    };
+
+    request.post({url:host + '/extension', formData: formData}, function (err, response, body) {
+      if (response === undefined || response.statusCode !== 200) {
+        console.log(("Unable to publish " + name + "! A server error occured.").red);
+        console.log(body + "(" + response.statusCode + ")");
+      } else {
+        console.log((name + " was published successfully!").green);
       }
-
-      utils.zip('.', name, function() {
-
-        var host;
-        var demo = false;
-        if (!demo) {
-          host = "https://sonavoice.com";
-        } else {
-          host = "http://localhost:3000";
-        }
-
-        var formData = {
-          guid: utils.getGUID(),
-          extension: fs.createReadStream(utils.lodir(name + '.zip')),
-          email: utils.getEmail(),
-          name: name,
-        };
-
-        request.post({url:host + '/extension', formData: formData}, function (err, response, body) {
-          if (response === undefined || response.statusCode !== 200) {
-            console.log(("Unable to publish " + name + "! A server error occured.").red);
-            console.log(body + "(" + response.statusCode + ")");
-          } else {
-            console.log((name + " was published successfully!").green);
-          }
-        });
-      });
-
-
-    } catch(e) {
-      console.log(e);
-      console.log('Invalid extension. Please check your syntax.'.red);
-      return;
-    }
-
+    });
   });
 
   function isValidFile(obj) {
--- template.js
+++ template.js
@@ -1,7 +1,13 @@
-{
-  "title": "extension",
-  "iconURL": "logo.png",
-  "commands": {
-    
-  }
+var request = require('request');
+
+module.exports = {
+  title: 'extension',
+  iconURL: 'logo.png',
+  commands: {
+    'to $1 say $2': function(cb, auth, args) {
+      var user = args[0];
+      var message = args[1];
+    }
+  },
+  sampleCommands: ['To Sarah say I\'m running late']
 }
